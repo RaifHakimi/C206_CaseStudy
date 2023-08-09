4adf7be7973fa096a24688264aa23619b562de10
joyyyyyyyyyyyyyyyyyy <22022096@myrp.edu.sg>
2023-08-09 16:47:19 +0800

    added helper and user class

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<User> userList = new ArrayList<User>();
 
 	}
 
---
+++ C206_CaseStudy/src/User.java
@@ -0,0 +1,58 @@
+
+public class User {
+	private String username;
+	private String email;
+	private String password;
+	private int mobile;
+	private String address;
+	
+	public User (String username, String email, String password,
+			int mobile, String address) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.mobile = mobile;
+		this.address = address;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername() {
+		this.username = username;
+	}
+	
+	public String getEmail() {
+		return username;
+	}
+	
+	public void setEmail() {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword() {
+		this.password = password;
+	}
+	
+	public int getMobile() {
+		return mobile;
+	}
+	
+	public void setMobile() {
+		this.mobile = mobile;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void Address() {
+		this.address = address;
+	}
+
+}
