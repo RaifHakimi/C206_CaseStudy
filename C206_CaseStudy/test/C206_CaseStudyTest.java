d144644b373d8a07cdcd50176d95edd862576aa7
joyyyyyyyyyyyyyyyyyy <22022096@myrp.edu.sg>
2023-08-10 23:57:18 +0800

    added test case for retrieveUser, still have some error that needs to be fixed

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -39,31 +39,59 @@ public class C206_CaseStudyTest {
 		//test that user list is not null and it is empty (boundary test) 
 		assertNotNull("test if there is valid user arraylist to add to", userList);
 		assertEquals("test that the user arraylist is empty", 0, userList.size());
-		//given that the list is empty, after adding 1 item, the size of the list is 1
+		//given that the list is empty, after adding 1 item, the size of the list is 1 (normal test)
 		C206_CaseStudy.addUser(userList, user1);
 		userList.add(user1);
 		assertEquals("test that the user arraylist is 1", 1, userList.size());
 		assertSame("test that user1 has been added into the arraylist", user1, userList.get(0));
 		
-		//add a user
+		//add a user (normal test)
 		C206_CaseStudy.addUser(userList, user2);
 		userList.add(user2);
 		assertEquals("test that the arraylist is now 2", 2, userList.size());
 		// test that the user just added is the same as the last item in the list
 		assertSame("test that user is added to the end of the list", user2, userList.get(1));
 		
-		//add a user that already exist in the system
+		//add a user that already exist in the system (err test)
 		C206_CaseStudy.addUser(userList, user1);
 		assertEquals("test that the user arraylist remains unchange", 2, userList.size());
 		
-		//add a user that has missing details
+		//add a user that has missing details (err test)
 		User user_missing = new User("", null, null, 0, null);
 		C206_CaseStudy.addUser(userList, user_missing);
 		assertEquals("test that the user arraylist size remains unchange", 2, userList.size());
 	}
 	
 	@Test
-	
+	public void testRetrieveAllUser() {
+		//test case 1
+		//test that user list is not null and is empty (boundary test)
+		assertNotNull("test if there is valid user arraylist to add to", userList);
+		assertEquals("test that the user arraylist is empty", 0, userList.size());
+		//attempt to retrieve users
+		String allUsers = C206_CaseStudy.retrieveAllUser(userList);
+		String testOutput = "";
+		//test if the output is empty
+		assertEquals("test that nothing is being displayed", testOutput, allUsers);
+		
+		//test case 2
+		C206_CaseStudy.addUser(userList, user1);
+		C206_CaseStudy.addUser(userList, user2);
+		userList.add(user1);
+		userList.add(user2);
+		//test that the list is not empty
+		assertEquals("test that the user arraylist size is 2", 2, userList.size());
+		//attempt to retrieve the users
+		allUsers = C206_CaseStudy.retrieveAllUser(userList);
+		testOutput = String.format("%-10s %-25s %-20s %-8d %-40s", "jontan", "jontan@gmail.com", "jon123", 90123456, "76 smith street");
+		testOutput += String.format("\n%-10s %-25s %-20s %-8d %-40s", "marylim", "marylim@gmail.com", "mary987", 80987654, "52 oak street");
+		user1 = new User("jontan", "jontan@gmail.com", "jon123", 90123456, "76 smith street");
+		user2 = new User("marylim", "marylim@gmail.com", "mary987", 80987654, "52 oak street");
+		//test that the details of the users is displayed correctly
+		//System.out.println(testOutput);
+		//System.out.println(allUsers);
+		assertEquals("test that the display is correct", testOutput, allUsers);
+	}
 	
 	
 	@After
