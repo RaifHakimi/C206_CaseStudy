9d86151a3086ba14a3510a1537ca4f5b5421fff3
joyyyyyyyyyyyyyyyyyy <22022096@myrp.edu.sg>
2023-08-14 02:18:58 +0800

    added service provider class (done by jianyang)

---
+++ C206_CaseStudy/src/Serviceprovider.java
@@ -0,0 +1,52 @@
+//done by jianyang
+public class Serviceprovider {
+	private String name;
+	private String description;
+	private String service;
+	private int contactnum;
+
+	public Serviceprovider(String name, String description, String service, int contactnum) {
+		this.name = name;
+		this.description = description;
+		this.service = service;
+		this.contactnum = contactnum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getService() {
+		return service;
+	}
+
+	public void setService(String service) {
+		this.service = service;
+	}
+
+	public int getContactnum() {
+		return contactnum;
+	}
+
+	public void setContactnum(int contactnum) {
+		this.contactnum = contactnum;
+	}
+
+	@Override
+	public String toString() {
+		return "ServiceProvider{" + "companyName='" + name + '\'' + ", Description='" + description + '\''
+				+ ", servicesOffered='" + service + '\'' + ", Contact Number='" + contactnum + '\'' + '}';
+	}
+}
