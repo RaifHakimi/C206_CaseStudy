058749b7142a54ac77369695e8765d856e8615f0
joyyyyyyyyyyyyyyyyyy <22022096@myrp.edu.sg>
2023-08-15 01:08:15 +0800

    added service provider class (done by jianyang)

--- C206_CaseStudy/src/Serviceprovider.java
+++ C206_CaseStudy/src/Serviceprovider.java
@@ -1,52 +1,67 @@
-//done by jianyang
 public class Serviceprovider {
 	private String name;
 	private String description;
 	private String service;
 	private int contactnum;
-
-	public Serviceprovider(String name, String description, String service, int contactnum) {
+	
+	public Serviceprovider (String name, String description, String service, int contactnum) {
 		this.name = name;
 		this.description = description;
 		this.service = service;
 		this.contactnum = contactnum;
 	}
-
+	
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
 	public String getDescription() {
 		return description;
 	}
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
+	
 	public String getService() {
 		return service;
 	}
-
 	public void setService(String service) {
 		this.service = service;
 	}
-
+	
 	public int getContactnum() {
 		return contactnum;
 	}
-
 	public void setContactnum(int contactnum) {
 		this.contactnum = contactnum;
 	}
-
+	
 	@Override
-	public String toString() {
-		return "ServiceProvider{" + "companyName='" + name + '\'' + ", Description='" + description + '\''
-				+ ", servicesOffered='" + service + '\'' + ", Contact Number='" + contactnum + '\'' + '}';
+    public String toString() {
+        return "ServiceProvider{" +
+                "companyName='" + name + '\'' +
+                ", Description='" + description + '\'' +
+                ", servicesOffered='" + service + '\'' +
+                ", Contact Number='" + contactnum + '\'' +
+                '}';
+    }
+
+	public static boolean addServiceprovider(Serviceprovider minProvider) {
+		
+		return false;
 	}
+
+	public static Serviceprovider viewServiceprovider(String string) {
+		
+		return null;
+	}
+
+	public static boolean deleteServiceprovider(String string) {
+		
+		return false;
+	}
+
 }
